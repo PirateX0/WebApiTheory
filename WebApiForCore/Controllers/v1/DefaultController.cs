f63211e351312084fe7aad2747a50ea69874a4f1
PirateX0 <295528322@qq.com>
2018-10-07 14:31:31 +0800

    20181007

---
+++ WebApiForCore/Controllers/v1/DefaultController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApiForCore.Controllers.v1
+{
+    [Produces("application/json")]
+    //[Route("api/Default")]
+    public class DefaultController : Controller
+    {
+        [HttpGet(nameof(Test))]
+        public string Test()
+        {
+            return "v1";
+        }
+
+    }
+}
